388488ef87f70e9de21d6936432bd974956c7273
João Henrique <123948786+kirigaya7447@users.noreply.github.com>
2023-11-01 11:34:29 -0300

    Update escondido.js

--- escondido.js
+++ escondido.js
@@ -1,7 +1,17 @@
-alert("Hello");
 
-function ola(){
-  alert("Ah");
-  alert("Vai");
-  alert("Cagar");
-} 
+function verSenha(){
+    let textoBtn = document.getElementById("verSenha").innerHTML;
+    if(textoBtn === "Mostrar"){
+        textoBtn = "Esconder";
+        document.getElementById("verSenha").innerHTML = textoBtn;
+        document.getElementById("senha").type = "text";
+    }
+    else{
+        textoBtn = "Mostrar";
+        document.getElementById("verSenha").innerHTML = textoBtn;
+        document.getElementById("senha").type = "password";
+    }
+}
+
+function verificar(){
+}
